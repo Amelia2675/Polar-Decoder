1e6d125c6893abad739cf840b5b4278cbef00d5e
Amelia2675 <apple.vividcute@gmail.com>
2023-03-15 19:04:56 +0800

    test

---
+++ 01_RTL/define.v
@@ -0,0 +1,13 @@
+`ifndef DEFINE_V
+`define DEFINE_V
+
+
+    //FSM
+    `define IDLE               0
+    `define INFO               1
+    `define DATA_FRO           2
+    `define DATA_DEC           3
+    `define DONE               4
+
+`endif
+
